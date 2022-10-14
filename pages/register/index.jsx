b7e688e68d5d3d9d3189ea496fd390d878f18c57
matheus jesse <matheus_jesse@hotmail.com>
2022-10-13 22:58:58 -0300

    feat: criando page de register

---
+++ pages/register/index.jsx
@@ -0,0 +1,15 @@
+import RegisterContainer from "./style";
+import Header from '../../components/Header'
+
+const Register = () => {
+  return ( 
+    <>
+      <Header />
+      <RegisterContainer>
+        <h1>Register</h1>
+      </RegisterContainer>
+    </>
+   );
+}
+
+export default Register;
