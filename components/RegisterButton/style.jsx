391f5c9a6f32e037446f0a5e2eadf71d70e57f6c
matheus jesse <matheus_jesse@hotmail.com>
2022-10-14 03:07:45 -0300

    feat: criando estilo base do componente RegisterBu

---
+++ components/RegisterButton/style.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const RegisterButtonContainer = styled.div`
+  
+  margin-bottom: 48px;
+
+  button {
+    width: 216px;
+    height: 60px;
+    left: 99px;
+    top: 776px;
+    background: #03213E;
+    border-radius: 50px;
+    font-weight: 500;
+    font-size: 28px;
+    line-height: 34px;
+    color: #FBFBFB;
+    border: none;
+  }
+
+`;
+
+export default RegisterButtonContainer;
