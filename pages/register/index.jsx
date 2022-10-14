7c51575a89d32f68f67440e0d0db4d13a646b6fa
matheus jesse <matheus_jesse@hotmail.com>
2022-10-14 03:08:29 -0300

    feat: criando page de registro de ToDos

--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -1,12 +1,14 @@
 import RegisterContainer from "./style";
 import Header from '../../components/Header'
+import RegisterFormContainer from '../../components/RegisterForm'
 
 const Register = () => {
   return ( 
     <>
       <Header />
       <RegisterContainer>
-        <h1>Register</h1>
+        <h1>Cadastrar ToDo</h1>
+        <RegisterFormContainer />
       </RegisterContainer>
     </>
    );
