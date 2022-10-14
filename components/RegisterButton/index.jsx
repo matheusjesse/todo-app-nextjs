4e0ad7cfbcef1a226c8c74acca11b993e5e4b77b
matheus jesse <matheus_jesse@hotmail.com>
2022-10-14 03:07:18 -0300

    feat: criando componente RegisterButton

---
+++ components/RegisterButton/index.jsx
@@ -0,0 +1,11 @@
+import RegisterButtonContainer from './style';
+
+const RegisterButton = () => {
+  return ( 
+    <RegisterButtonContainer>
+      <button type="button">Salvar</button>
+    </RegisterButtonContainer>
+   );
+}
+ 
+export default RegisterButton;
