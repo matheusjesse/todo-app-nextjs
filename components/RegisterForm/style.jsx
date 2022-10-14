fca0f50707af20bae45f8e6a06f08a064a513ab9
matheus jesse <matheus_jesse@hotmail.com>
2022-10-14 03:08:11 -0300

    style: criando estilo base RegisterForm

---
+++ components/RegisterForm/style.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const RegisterFormContainer = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export default RegisterFormContainer;
