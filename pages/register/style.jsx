3ae63c895405f3791ce9bb078e03916b234a5851
matheus jesse <matheus_jesse@hotmail.com>
2022-10-14 03:08:56 -0300

    style: criando estilo base da page de registro

--- pages/register/style.jsx
+++ pages/register/style.jsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 
 const RegisterContainer = styled.div`
+  text-align: center;
+  
   h1 {
     color: red;
+    margin-bottom: 14px;
+    color:  var(--blue);
   }
 `;
 
