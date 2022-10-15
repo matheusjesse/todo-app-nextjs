cf6e2cfbaae2669953c12365cf4cac1a5a3bde91
matheus jesse <matheus_jesse@hotmail.com>
2022-10-14 23:46:30 -0300

    feat: criando states para cada componente do form

--- components/RegisterForm/index.jsx
+++ components/RegisterForm/index.jsx
@@ -3,13 +3,18 @@ import RegisterFormContainer from './style';
 import DayOfTheWeekContainer from '../../components/DayOfTheWeek';
 import NoteInpute from '../../components/NoteInput';
 import RegisterButton from '../../components/RegisterButton';
+import { useEffect, useState } from 'react';
 
 const RegisterForm = () => {
+  const [dayPeriod, setDayPeriod] = useState([]);
+  const [dayOfTheWeek, setDayOfTheWeek] = useState([]);
+  const [noteText, setNoteText] = useState('');
+
   return ( 
     <RegisterFormContainer>
-      <DayPeriod />
-      <DayOfTheWeekContainer />
-      <NoteInpute />
+      <DayPeriod setDayPeriod={setDayPeriod} dayPeriod={dayPeriod} />
+      <DayOfTheWeekContainer setDayOfTheWeek={setDayOfTheWeek} dayOfTheWeek={dayOfTheWeek}/>
+      <NoteInpute noteText={noteText} setNoteText={setNoteText}/>
       <RegisterButton />
     </RegisterFormContainer>
    );
