48976933fe3623cec8c14dd3f6f675f8ae9d57ad
matheus jesse <matheus_jesse@hotmail.com>
2022-10-15 01:54:55 -0300

    chore: setando button para desabilitado

--- components/RegisterButton/index.jsx
+++ components/RegisterButton/index.jsx
@@ -1,11 +1,12 @@
 import RegisterButtonContainer from './style';
 
-const RegisterButton = ({ saveTodo }) => {
+const RegisterButton = ({ saveTodo, buttonDisabled }) => {
   return ( 
     <RegisterButtonContainer>
       <button
         type="button"
         onClick={saveTodo}
+        disabled={buttonDisabled}
       >
         Salvar
       </button>
