c77e4deb1e746cceb2e0126a901792bd9b5b2582
matheus jesse <matheus_jesse@hotmail.com>
2022-10-14 23:48:59 -0300

    feat: localStorage para armazenar os Todos

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,7 +1,13 @@
 import HomeContainer from "./style";
 import Header from '../../components/Header'
+import { useEffect } from "react";
 
 const Home = () => {
+
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify([]));
+  }, [])
+
   return ( 
     <>
       <Header />
