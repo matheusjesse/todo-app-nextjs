b3aacc4b7c1a953ad93160c103d4659365c90f99
matheus jesse <matheus_jesse@hotmail.com>
2022-10-16 00:20:22 -0300

    feat: componente todoList para listar os todos

--- components/TodoList/index.jsx
+++ components/TodoList/index.jsx
@@ -1,8 +1,34 @@
+import { useState, useEffect } from 'react';
 import TodoListContainer from './style';
 
-function TodoList() {
+function TodoList({ dayPeriod }) {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem('todoList'));
+    const newTodo = [];
+    data.forEach((item) => item.dayPeriod
+      .find((element) => element === dayPeriod && newTodo.push(item)));
+    setTodos(newTodo);
+  }, []);
+
   return (
-    <TodoListContainer />
+    <TodoListContainer>
+      {
+          todos && todos.map(({ noteText, id }) => (
+            <label htmlFor={`todo-${id}`} key={id}>
+              <input
+                data-testid="day-of-the-week-id"
+                type="checkbox"
+                id={`todo-${id}`}
+                name={`todo-${id}`}
+                value={noteText}
+              />
+              {noteText}
+            </label>
+          ))
+        }
+    </TodoListContainer>
   );
 }
 
