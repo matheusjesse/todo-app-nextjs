fe8b44dfa6e15ed52af6a9c68307ff860942f57f
matheus jesse <matheus_jesse@hotmail.com>
2022-10-16 00:20:31 -0300

    style: estilo do componente todoList

--- components/TodoList/style.jsx
+++ components/TodoList/style.jsx
@@ -1,7 +1,31 @@
 import styled from 'styled-components';
 
 const TodoListContainer = styled.div`
-  
+  width: 94vw;
+  margin-bottom: 12px;
+  border-radius: 8px;
+  background: var(--gray);
+  box-shadow: 0px 0px 2px 0px var(--blue);
+  color: var(--blue);
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 12px 0px 12px 0px;
+
+  input {
+    margin-right: 8px;
+    margin-left: 12px;
+  }
+
+  label {
+    width: 90%;
+    height: 42px;
+    display: flex;
+    align-items: center;
+    box-shadow: 0px 1px 0px -1px #000000;
+    border-radius: 0px 2px 2px 0px;
+  }
+
 `;
 
 export default TodoListContainer;
